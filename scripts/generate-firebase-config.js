5ef0dade10c910660f8a6fa1c77e606b9c09ae03
Caio Moizes Santos <engcaiomoizes@gmail.com>
2025-08-19 14:01:06 -0300

    Inclusão de template de Firebase Config para deploy na Vercel

---
+++ scripts/generate-firebase-config.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+
+const config = `
+export const firebaseConfig = {
+    apiKey: '${process.env.FIREBASE_API_KEY}',
+    authDomain: '${process.env.FIREBASE_AUTH_DOMAIN}',
+    projectId: '${process.env.FIREBASE_PROJECT_ID}',
+    storageBucket: '${process.env.FIREBASE_STORAGE_BUCKET}',
+    messagingSenderId: '${process.env.FIREBASE_MESSAGING_SENDER_ID}',
+    appId: '${process.env.FIREBASE_APP_ID}'
+};
+`;
+
+fs.writeFileSync('./src/environments/firebase.config.ts', config);
+console.log('firebase.config.ts gerado com sucesso!');
