6717f2cb600105b14e875c327bd16c3c0a52ae74
Caio Moizés Santos <138253578+engcaiomoizes@users.noreply.github.com>
2025-08-19 13:37:09 -0300

    Delete src/environments/firebase.config.ts

--- src/environments/firebase.config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyBsGKi4HEqAyGSqDKMOC6yTAXV-pW35sSg",
-    authDomain: "spendingcalculator-4b36f.firebaseapp.com",
-    projectId: "spendingcalculator-4b36f",
-    storageBucket: "spendingcalculator-4b36f.firebasestorage.app",
-    messagingSenderId: "32415999873",
-    appId: "1:32415999873:web:db7cf32fbff045b6e8ea80"
-};
